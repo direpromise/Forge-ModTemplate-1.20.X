0cbb85a52d72a9c678813e797e93c38aad66a0e8
direpromise <holyfugazi@gmail.com>
2023-12-02 00:18:33 +0200

    BlockEntityRenderer.

--- src/main/java/net/fuii/tutorialmod/event/ModEventBusClientEvents.java
+++ src/main/java/net/fuii/tutorialmod/event/ModEventBusClientEvents.java
@@ -1,6 +1,8 @@
 package net.fuii.tutorialmod.event;
 
 import net.fuii.tutorialmod.TutorialMod;
+import net.fuii.tutorialmod.block.entity.ModBlockEntities;
+import net.fuii.tutorialmod.block.entity.renderer.GemPolishingBlockEntityRenderer;
 import net.fuii.tutorialmod.entity.client.ModModelLayers;
 import net.fuii.tutorialmod.entity.client.RhinoModel;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,5 +15,10 @@ public class ModEventBusClientEvents {
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.GEM_POLISHING_BE.get(), GemPolishingBlockEntityRenderer::new);
     }
 }
